cd5f394823981252f321916d84a85f2ca13e2b3d
Ajay209Git <ajay209patel@gmail.com>
2026-02-12 15:54:24 +0530

    Create lib/utils/constants.dart file with game constants and configuration

---
+++ lib/utils/constants.dart
@@ -0,0 +1,13 @@
+// Constants and Configuration for Survivor Shooter Game
+
+// Game Constants
+const int maxPlayers = 100;
+const int startingLives = 3;
+const int maxItems = 50;
+const double gameTimeLimit = 30.0; // in minutes
+
+// Configurations
+const String gameVersion = '1.0.0';
+const String gameTitle = 'Survivor Shooter';
+const List<String> supportedLanguages = ['English', 'Spanish', 'French'];
+const bool enableSound = true;
