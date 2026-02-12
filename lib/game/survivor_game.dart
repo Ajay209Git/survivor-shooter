4fba4f059d64fe679bd5b8e692580270482c0420
Ajay209Git <ajay209patel@gmail.com>
2026-02-12 15:54:52 +0530

    Create survivor_game.dart with Flame game engine setup

---
+++ lib/game/survivor_game.dart
@@ -0,0 +1,13 @@
+import 'package:flame/game.dart';
+
+class SurvivorGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    // Add your game initialization code here
+  }
+
+  @override
+  void update(double dt) {
+    // Add your game update logic here
+  }
+}
